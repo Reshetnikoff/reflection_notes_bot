23f9c918d5ba6bccea3f69827613eadddb1abd71
Kirill Reshetnikov <kirill.reshetnikoff@gmail.com>
2020-04-22 00:53:39 +0300

    Add /write_results and /load_results function

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 config = {
-    "TOKEN" : "TOKEN",
-    "ADRESS" : "ADRESS",
-    "PORT" : "PORT"
+    "TOKEN" : "947890754:AAEJhxrDyaQMT1tly5GuOFmHI_3gzZekl_0",
+    "ADRESS" : "/127.0.0.",
+    "PORT" : "9150"
 }
